b82d9be9bd452050e9d53777cc25b55610bfa7e6
Karthik M R <karthik2018mr@gmail.com>
2025-09-05 18:26:44 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/background-watermark.tsx
+++ src/components/background-watermark.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { VasaviMaathaIcon } from "@/components/icons/vasavi-maatha-icon";
+import { useState, useEffect } from "react";
 
 export default function BackgroundWatermark() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="fixed inset-0 z-[-1] overflow-hidden">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
