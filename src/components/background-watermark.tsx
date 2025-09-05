63ef2befc965776474cd5fa8b0d950da664a6523
Karthik M R <karthik2018mr@gmail.com>
2025-09-05 18:25:31 +0000

    no the image seems not at all visible so make it more visible in the bac

--- src/components/background-watermark.tsx
+++ src/components/background-watermark.tsx
@@ -6,7 +6,7 @@ export default function BackgroundWatermark() {
   return (
     <div className="fixed inset-0 z-[-1] overflow-hidden">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <VasaviMaathaIcon className="h-[80vmin] w-[80vmin] text-foreground/5 opacity-50" />
+        <VasaviMaathaIcon className="h-[80vmin] w-[80vmin] text-foreground/10" />
       </div>
     </div>
   );
